ee69ca1701c00808dcc01f6275f414fb3746fab1
crramires <crramiress@gmail.com>
2025-06-30 15:58:25 -0300

    Creating the contact model and customizing django admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'email',
+    ordering = 'id',
+    list_filter = 'created_date',
+    search_fields = 'first_name', 'last_name',
+    list_per_page = 10
+    list_max_show_all = 100
