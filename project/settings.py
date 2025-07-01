57cc337191bc687373f4dd9c49aa5b37f173f43c
crramires <crramiress@gmail.com>
2025-07-01 16:09:51 -0300

    Styling the contacts table and inserting data into the table

--- project/settings.py
+++ project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -55,9 +55,7 @@ ROOT_URLCONF = "project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [
-            BASE_DIR / 'base_templates'
-        ],
+        "DIRS": [os.path.join(BASE_DIR, 'base_templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -117,10 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = (
-    BASE_DIR / 'base_static',
-)
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'base_static'),]
 STATIC_ROOT = BASE_DIR / 'static'  # collectstatic
 
 MEDIA_URL = 'media/'
