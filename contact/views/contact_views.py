ba2934f05db609f1998420c6a75014271bed132b
crramires <crramiress@gmail.com>
2025-07-02 17:37:22 -0300

    Implementing HTML header and search route

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
+from django.db.models import Q
+
 
 
 def index(request):
@@ -12,6 +14,29 @@ def index(request):
         'contact/index.html',
         context
     )
+
+def search(request):
+
+    search_value = request.GET.get('q', '').strip()
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects.filter(
+        Q(first_name__icontains=search_value) | 
+        Q(last_name__icontains=search_value) |
+        Q(email__icontains=search_value)
+    )
+
+    context = {
+        'contacts': contacts,
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
 
 def contact(request, contact_id):
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
