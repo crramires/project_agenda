d5a363fc10f93530c762dd653dc85e6095d30ff1
crramires <crramiress@gmail.com>
2025-07-07 18:13:36 -0300

    Saving contact data in the database and validating the data

---
+++ contact/forms.py
@@ -0,0 +1,53 @@
+from django import forms
+from contact.models import Contact
+from django.core.exceptions import ValidationError
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name', 'last_name', 'phone', 'email', 'description', 'category'
+        )
+
+    def clean(self):
+        # cleaned_data = self.cleaned_data
+
+        # self.add_error(
+        #     'first_name',
+        #     ValidationError(
+        #         'Mensagem de erro',
+        #         code='invalid'
+        #     )
+        # )
+
+        return super().clean()
+    
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if not first_name:
+            raise ValidationError(
+                'O campo first_name é obrigatorio.',
+                code='invalid'
+            )
+        if any(char.isdigit() for char in first_name):
+            raise  ValidationError(
+                'Deve ser um nome valido.',
+                code='invalid'
+            )
+        return first_name
+    
+    def clean_last_name(self):
+        last_name = self.cleaned_data.get('last_name')
+
+        if not last_name:
+            raise ValidationError(
+                'O campo last_name é obrigatorio.',
+                code='invalid'
+            )
+        if any(char.isdigit() for char in last_name):
+            raise  ValidationError(
+                'Deve ser um nome valido.',
+                code='invalid'
+            )
+        return last_name
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,23 +1,20 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from contact.models import Contact
-from django.db.models import Q
-from django.core.paginator import Paginator
-from django import forms
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = (
-            'first_name', 'last_name', 'phone', 
-        )
+from django.shortcuts import render, redirect
+from contact.forms import ContactForm
 
 
 def create(request):
     if request.method == 'POST': 
+        form = ContactForm(request.POST)
         context = {
-            'form': ContactForm(request.POST)
+            'form': form
         }
         
+        if form.is_valid():
+            form.save()
+            return redirect(
+                'contact:create'
+            )
+
         return render(
             request,
             'contact/create.html',
